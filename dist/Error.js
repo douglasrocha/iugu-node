820bc066fffdee5b76180aef1476bdd20641d3ad
Douglas Rocha <douglasbellonrocha@gmail.com>
2018-04-19 18:41:09 -0300

    Não vi que faltava um super() =p

--- dist/Error.js
+++ dist/Error.js
@@ -1,73 +1,134 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _utils = require('./utils');
 
 var _utils2 = _interopRequireDefault(_utils);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/*
-export default class _Error extends Error {
-  constructor () {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _Error = function (_Error2) {
+  _inherits(_Error, _Error2);
+
+  function _Error() {
+    _classCallCheck(this, _Error);
+
+    var _this = _possibleConstructorReturn(this, (_Error.__proto__ || Object.getPrototypeOf(_Error)).call(this));
+
+    _this.type = 'IuguError', _this.populate = function (raw) {
+      _this.type = _this.type;
+      _this.rawType = raw.type;
+      _this.code = raw.code;
+      _this.param = raw.param;
+      _this.message = raw.message;
+      _this.detail = raw.detail;
+      _this.raw = raw;
+    };
+    return _this;
   }
-}
-*/
 
-/**
- * Generic Error class to wrap any errors returned by iugu-node
- */
-function _Error(raw) {
-  this.populate.apply(this, arguments);
-}
+  _createClass(_Error, [{
+    key: 'generate',
+    value: function generate(rawIuguError) {
+      switch (rawIuguError.type) {
+        case 'card_error':
+          return new IuguCardError(rawIuguError);
+        case 'invalid_request_error':
+          return new IuguInvalidRequestError(rawIuguError);
+        case 'api_error':
+          return new IuguAPIError(rawIuguError);
+      }
+      return new _Error('Generic', 'Unknown Error');
+    }
+  }]);
 
-// Extend Native Error
-_Error.prototype = Object.create(Error.prototype);
+  return _Error;
+}(Error);
 
-_Error.prototype.type = 'GenericError';
-_Error.prototype.populate = function (type, message) {
-  this.type = type;
-  this.message = message;
-};
+exports.default = _Error;
 
-_Error.extend = _utils2.default.protoExtend;
+var IuguCardError = function (_Error3) {
+  _inherits(IuguCardError, _Error3);
 
-/**
- * Create subclass of internal Error class
- * (Specifically for errors returned from Iugu's REST API)
- */
-var IuguError = _Error.IuguError = _Error.extend({
-  type: 'IuguError',
-  populate: function populate(raw) {
+  function IuguCardError() {
+    _classCallCheck(this, IuguCardError);
 
-    // Move from prototype def (so it appears in stringified obj)
-    this.type = this.type;
-    this.rawType = raw.type;
-    this.code = raw.code;
-    this.param = raw.param;
-    this.message = raw.message;
-    this.detail = raw.detail;
-    this.raw = raw;
+    var _this2 = _possibleConstructorReturn(this, (IuguCardError.__proto__ || Object.getPrototypeOf(IuguCardError)).call(this));
+
+    _this2.type = 'IuguCardError';
+    return _this2;
   }
-});
 
-/**
- * Helper factory which takes raw iugu errors and outputs wrapping instances
- */
-IuguError.generate = function (rawIuguError) {
-  switch (rawIuguError.type) {
-    case 'card_error':
-      return new _Error.IuguCardError(rawIuguError);
-    case 'invalid_request_error':
-      return new _Error.IuguInvalidRequestError(rawIuguError);
-    case 'api_error':
-      return new _Error.IuguAPIError(rawIuguError);
+  return IuguCardError;
+}(_Error);
+
+var IuguInvalidRequestError = function (_Error4) {
+  _inherits(IuguInvalidRequestError, _Error4);
+
+  function IuguInvalidRequestError() {
+    _classCallCheck(this, IuguInvalidRequestError);
+
+    var _this3 = _possibleConstructorReturn(this, (IuguInvalidRequestError.__proto__ || Object.getPrototypeOf(IuguInvalidRequestError)).call(this));
+
+    _this3.type = 'IuguInvalidRequest';
+    return _this3;
   }
-  return new _Error('Generic', 'Unknown Error');
-};
 
-// Specific Stripe Error types:
-_Error.IuguCardError = IuguError.extend({ type: 'IuguCardError' });
-_Error.IuguInvalidRequestError = IuguError.extend({ type: 'IuguInvalidRequest' });
-_Error.IuguAPIError = IuguError.extend({ type: 'IuguAPIError' });
-_Error.IuguAuthenticationError = IuguError.extend({ type: 'IuguAuthenticationError' });
-_Error.IuguConnectionError = IuguError.extend({ type: 'IuguConnectionError' });
+  return IuguInvalidRequestError;
+}(_Error);
+
+var IuguAPIError = function (_Error5) {
+  _inherits(IuguAPIError, _Error5);
+
+  function IuguAPIError() {
+    _classCallCheck(this, IuguAPIError);
+
+    var _this4 = _possibleConstructorReturn(this, (IuguAPIError.__proto__ || Object.getPrototypeOf(IuguAPIError)).call(this));
+
+    _this4.type = 'IuguAPIError';
+    return _this4;
+  }
+
+  return IuguAPIError;
+}(_Error);
+
+var IuguAuthenticationError = function (_Error6) {
+  _inherits(IuguAuthenticationError, _Error6);
+
+  function IuguAuthenticationError() {
+    _classCallCheck(this, IuguAuthenticationError);
+
+    var _this5 = _possibleConstructorReturn(this, (IuguAuthenticationError.__proto__ || Object.getPrototypeOf(IuguAuthenticationError)).call(this));
+
+    _this5.type = 'IuguAuthenticationError';
+    return _this5;
+  }
+
+  return IuguAuthenticationError;
+}(_Error);
+
+var IuguConnectionError = function (_Error7) {
+  _inherits(IuguConnectionError, _Error7);
+
+  function IuguConnectionError() {
+    _classCallCheck(this, IuguConnectionError);
+
+    var _this6 = _possibleConstructorReturn(this, (IuguConnectionError.__proto__ || Object.getPrototypeOf(IuguConnectionError)).call(this));
+
+    _this6.type = 'IuguConnectionError';
+    return _this6;
+  }
+
+  return IuguConnectionError;
+}(_Error);
