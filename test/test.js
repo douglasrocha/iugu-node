3038fe39b3be0494a25b62d07e0ccb3716bf8921
Douglas Rocha <douglas@invest.pro>
2018-04-20 08:14:08 -0300

    Adicionados testes de instancia + correção de set de api key;

--- test/test.js
+++ test/test.js
@@ -1,13 +1,38 @@
-import assert from 'assert';
+import chai from 'chai';
+import chaiString from 'chai-string';
 import Iugu from '../lib/iugu';
 import { ApiKey } from './TestKey';
 
+// Chai Configurations
+chai.use(chaiString);
+const should = chai.should();
+
 describe('Main iugu object creation', () => {
   describe('When instantiated', () => {
+    const iugu = new Iugu(ApiKey, 'latest');
+    
     it('should be defined and not null', () => {
-      const iugu = new Iugu(ApiKey, 'latest');
-      assert.notEqual(iugu, undefined);
-      assert.notEqual(iugu, null);
+      should.exist(iugu);
+    });
+
+    it('needs to have some default values as attributes', () => {
+      iugu.DEFAULT_HOST.should.be.equal('api.iugu.com');
+      iugu.DEFAULT_PORT.should.be.equal('443');
+      should.exist(iugu.resources);
+      iugu.resources.should.be.an('object');
+      iugu._api.should.be.an('object');
+    });
+
+    it('needs to have a base64 encoding starting with Basic', () => {
+      const auth = iugu.getApiField('auth');
+      should.exist(auth);
+      auth.should.startWith('Basic ');
+    });
+
+    it('should contain an instance of IuguResource', () => {
+      should.exist(iugu.IuguResource);
+      iugu.IuguResource.should.be.an('object');
+      iugu.IuguResource._iugu.should.be.equal(iugu);
     });
   });
 });
